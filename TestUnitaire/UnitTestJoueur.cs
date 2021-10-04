5b1706e9d688d85833a8ccb69327afc7b3ce5191
Tdartigues <73830061+Tdartigues@users.noreply.github.com>
2021-10-04 15:26:31 +0200

    Errarum Test Joueur

---
+++ TestUnitaire/UnitTestJoueur.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TestUnitaire
+{
+    [TestClass]
+    public class UnitTestJoueur
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
